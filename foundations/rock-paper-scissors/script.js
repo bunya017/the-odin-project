7641bd249040907779517467a09c7b30e16c5e54
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-05-02 10:18:01 +0100

    refactor(rock-paper-scissors): Add game()

--- foundations/rock-paper-scissors/script.js
+++ foundations/rock-paper-scissors/script.js
@@ -23,3 +23,22 @@ function playRound (playerSelection, computerSelection) {
     return 'You win! Rock beats Paper'
   }
 }
+
+function game () {
+  let player = 0
+  let computer = 0
+  while ((player + computer) < 5){
+    const playerSelection = prompt('Rock, Paper, Scissors:')
+    const computerSelection = computerPlay()
+    const score = playRound(playerSelection, computerSelection)
+    if (score.indexOf('You win!') > -1) {
+      player++
+    } else if (score.indexOf('You lose!') > -1) {
+      computer++
+    }
+  }
+  const winner = (player > computer) ? 'You win!' : 'You lose'
+  console.log(`${winner} ${player}:${computer}`)
+}
+
+game()
