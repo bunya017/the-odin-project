e8c870709c97397ea59f153f27bd9e758bc4bfd1
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2022-05-06 23:59:59 +0100

    refactor(calculator): Add addToDisplay()

--- foundations/calculator/calculator.js
+++ foundations/calculator/calculator.js
@@ -1,3 +1,4 @@
+let DISPLAY = ''
 function add (num1, num2) {
   return num1 + num2
 }
@@ -13,4 +14,17 @@ function multiply (num1, num2) {
 function divide (num1, num2) {
   let division = num1 / num2
   return (division == 'Infinity') ? 'Hehehe... SIUUU!!!' : division
+}
+
+function addToDisplay (num) {
+  if ((num === '.') && (DISPLAY.indexOf('.') === -1)) {
+    if (DISPLAY.length === 0) {
+      DISPLAY += `0${num}`
+    } else {
+      DISPLAY += `${num}`
+    }
+  } else if (num !== '.') {
+    DISPLAY += `${num}`
+  }
+  document.getElementById('display').innerText = DISPLAY
 }
